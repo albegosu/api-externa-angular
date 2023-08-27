3e7f753b1104a68383b404150551958aa2c495d2
Alberto Glez <albertoglez@Alberto-Glez.local>
2023-08-27 23:52:53 +0200

    Service/Interface create

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -1,2 +1,7 @@
 export interface User {
+  first_name: string;
+  last_name: string;
+  username: string;
+  email: string;
+  image: string;
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  httpClient = inject(HttpClient)
+  private url: string = "https://peticiones.online/api/users";
+
   constructor() { }
+
+  getAll(): Promise<any[]> {
+    return lastValueFrom(this.httpClient.get<any[]>(this.url))
+  }
+
 }
