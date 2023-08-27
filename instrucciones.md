008245cbf8687a8feaa2c644ee9730c53bc2c435
Alberto Glez <albertoglez@Alberto-Glez.local>
2023-08-27 23:32:12 +0200

    Pages, components, services, interfaces structure

---
+++ instrucciones.md
@@ -0,0 +1,31 @@
+Rutas:
+
+  /home:
+  /user/1: 
+  /newuser:
+  /updateuser/1:
+
+Componentes reutilizables:
+
+ /card
+ /deleteBtn
+ /updateBtn
+ /returnBtn
+ /form
+
+
+Interface: 
+  userInterface:
+    "first_name":
+    "last_name":
+    "username":
+    "email": 
+    "image": 
+
+Servicio:
+  GET https://peticiones.online/api/users
+  GET https://peticiones.online/api/users/IDUSUARIO
+  POST https://peticiones.online/api/users
+  PUT https://peticiones.online/api/users/IDUSUARIO
+  DELETE https://peticiones.online/api/users/IDUSUARIO
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { FormComponent } from './components/form/form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserProfileComponent,
+    FormComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
